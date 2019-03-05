e23dcfee1f2586fd14489dad85d26e827c55646e
Robin G.F. Mamie <robin@mamie.one>
2019-03-05 22:48:52 +0100

    Clean code Plane.cpp

--- assignment_1/src/Plane.cpp
+++ assignment_1/src/Plane.cpp
@@ -35,26 +35,27 @@ intersect(const Ray& _ray,
           vec3&      _intersection_normal,
           double&    _intersection_t ) const
 {
-	const vec3 &rayDir = _ray.direction;
-    const vec3 &rayOrigin = _ray.origin;
-    double dotRayDirNormal = dot(rayDir, normal);
+	const vec3        &dir = _ray.direction;
+    double dotRayDirNormal = dot(dir, normal);
 
     _intersection_t = NO_INTERSECTION;
 
-    //check if parallel
-    if(dotRayDirNormal == 0){
-		return false;		
+    // Check if plane perpendicular to ray.
+    if(dotRayDirNormal == 0.0) {
+		return false;
 	}
-	
-	//if not, compute intersection
-	const vec3   oc = _ray.origin - center;
-	double t = dot(normal, center - rayOrigin) / dotRayDirNormal;
-	
-	//check if solution is in front of the viewer
-	if(t < 0){
-		return  false;
+
+	// If not, compute intersection.
+    const vec3 &origin = _ray.origin;
+	const vec3     &oc = origin - center;
+
+	double t = dot(normal, -oc) / dotRayDirNormal;
+
+	// Check if solution is in front of the viewer.
+	if(t < 0) {
+		return false;
 	}
-	
+
 	_intersection_t = t;
 	_intersection_point  = _ray(_intersection_t);
 	_intersection_normal = normal;
