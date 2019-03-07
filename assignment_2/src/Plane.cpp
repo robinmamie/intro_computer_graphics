dde980f743090a1094ee45cf937f40c2ff28a1bd
Montial Charline <charline.montial@epfl.ch>
2019-03-07 09:37:28 +0100

    Add previous assignment implementation for Cylinder and Plane (as asked)

--- assignment_2/src/Plane.cpp
+++ assignment_2/src/Plane.cpp
@@ -35,9 +35,32 @@ intersect(const Ray& _ray,
           vec3&      _intersection_normal,
           double&    _intersection_t ) const
 {
-    // \todo Copy your assignment 1 solution here.
+	const vec3        &dir = _ray.direction;
+    double dotRayDirNormal = dot(dir, normal);
 
-    return false;
+    _intersection_t = NO_INTERSECTION;
+
+    // Check if plane perpendicular to ray.
+    if(dotRayDirNormal == 0.0) {
+		return false;
+	}
+
+	// If not, compute intersection.
+    const vec3 &origin = _ray.origin;
+	const vec3     &oc = origin - center;
+
+	double t = dot(normal, -oc) / dotRayDirNormal;
+
+	// Check if solution is in front of the viewer.
+	if(t < 0) {
+		return false;
+	}
+
+	_intersection_t = t;
+	_intersection_point  = _ray(_intersection_t);
+	_intersection_normal = normal;
+
+    return true;
 }
 
 
